e6381d49563c576df5e42692493fefb16c438d4c
rbaliwal00 <rajan18baliwal@gmail.com>
2021-08-10 10:40:43 +0530

    Adding Bootstrap for data. Closes #14

--- pet-clinic-data/src/main/java/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/map/OwnerServiceMap.java
@@ -3,10 +3,9 @@ package map;
 import java.util.Set;
 
 import com.example.springpetclinic.model.Owner;
+import services.OwnerService;
 
-import services.CrudService;
-
-public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long>{
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements OwnerService {
 
     @Override
     public Set<Owner> findAll() {
@@ -31,5 +30,10 @@ public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements 
     @Override
     public Owner findById(Long id) {
         return super.findById(id);
+    }
+
+    @Override
+    public Owner findByLastName(String lastName) {
+        return null;
     }
 }
