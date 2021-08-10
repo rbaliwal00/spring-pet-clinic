2555fc17987d1ff2c141661d6a8a2421b01c2dce
rbaliwal00 <rajan18baliwal@gmail.com>
2021-08-10 11:32:39 +0530

    owners list view failing

--- pet-clinic-data/src/main/java/services/OwnerService.java
+++ pet-clinic-data/src/main/java/services/OwnerService.java
@@ -1,7 +1,10 @@
 package services;
 
+import java.beans.JavaBean;
+
 import com.example.springpetclinic.model.Owner;
 
+@JavaBean
 public interface OwnerService extends CrudService<Owner, Long> {
 	
 	Owner findByLastName(String lastName);
