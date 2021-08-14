f3d9a4354361d22d45f2eee968e7e482a913370b
rbaliwal00 <rajan18baliwal@gmail.com>
2021-08-14 21:04:36 +0530

    updated owner with JPA entity mapping. Closes #36

--- pet-clinic-data/src/main/java/com/example/springpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/example/springpetclinic/model/Pet.java
@@ -1,12 +1,24 @@
 package com.example.springpetclinic.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table(name="pets")
 public class Pet extends BaseEntity{
 
+    @Column(name="name")
     private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "type_id")
     private PetType petype;
+
+    @ManyToOne
+    @JoinColumn(name="owner_id")
     private Owner owner;
+
+    @Column(name="birth_date")
     private LocalDate birthDate;
 
     public String getName() {
