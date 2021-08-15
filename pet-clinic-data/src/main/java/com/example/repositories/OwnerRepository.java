8cf4d7608ea66e558173ee57708f5cd730c3fb2a
rbaliwal00 <rajan18baliwal@gmail.com>
2021-08-15 13:12:58 +0530

    implemented repos. Closes #39

---
+++ pet-clinic-data/src/main/java/com/example/repositories/OwnerRepository.java
@@ -0,0 +1,8 @@
+package com.example.repositories;
+
+import com.example.springpetclinic.model.Owner;
+import org.springframework.data.repository.CrudRepository;
+
+public interface OwnerRepository extends CrudRepository<Owner, Long> {
+
+}
