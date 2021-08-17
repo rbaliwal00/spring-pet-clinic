6ab68c2b5f70ac28a5f1c86546a54ddbb0c28137
rbaliwal00 <rajan18baliwal@gmail.com>
2021-08-17 17:23:30 +0530

    added tests. Closes #49

--- pet-clinic-web/src/test/java/springpetclinic/SpringPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/springpetclinic/SpringPetClinicApplicationTests.java
@@ -1,11 +1,15 @@
 package springpetclinic;
 
+import com.example.springpetclinic.SpringPetClinicApplication;
 import org.junit.jupiter.api.Disabled;
 //import org.junit.Test;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@SpringBootTest
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = {SpringPetClinicApplication.class})
 @Disabled
 class SpringPetClinicApplicationTests {
 
